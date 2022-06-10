d4bf68959c740efde599ef7ad7ad68681ba54579
Sanskriti <87578327+ss1208@users.noreply.github.com>
2022-06-11 00:46:11 +0530

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from "react";
 import "./Home.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CarouselContainer from "./Carousel_Home/CarouselContainer";
+import Cards from "./Cards/Cards";
 
 export default function Home() {
   return (
-    <div className='home_body'>this is home</div>
-  )
+    <div>
+    <div className="home_body">
+      <CarouselContainer />
+    </div>
+    <div className="updates">
+    <Cards />
+  </div>
+  </div>
+
+  );
 }
