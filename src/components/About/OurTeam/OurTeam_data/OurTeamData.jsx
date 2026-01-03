dcc53bbf9120facc12fe7af6f1e4315177920148
The-Aethereal <suryanshctf05@gmail.com>
2026-01-03 20:45:46 +0530

    Team-update

--- src/components/About/OurTeam/OurTeam_data/OurTeamData.jsx
+++ src/components/About/OurTeam/OurTeam_data/OurTeamData.jsx
@@ -1,244 +1,125 @@
 //Image Import
-import secy from "../../../../Assets/OurTeam_imgs/OurTeam_photos/secreteries/Secy.jpg";
-import AddSecy from "../../../../Assets/OurTeam_imgs/OurTeam_photos/secreteries/Add-Secy.jpg";
-import AlumSecy from "../../../../Assets/OurTeam_imgs/OurTeam_photos/secreteries/Alum-outreach-Secy.jpg";
-import DesignSecy from "../../../../Assets/OurTeam_imgs/OurTeam_photos/secreteries/Design-Secy.jpg";
-import EdSecy from "../../../../Assets/OurTeam_imgs/OurTeam_photos/secreteries/Editorial-Secy.jpg";
-import OpSecy from "../../../../Assets/OurTeam_imgs/OurTeam_photos/secreteries/operations-Secy.jpg";
+import s5 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/2025/s.jpg";
+import as5 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/2025/as.jpg";
+import jso5 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/2025/jso.jpg";
+import jswd5 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/2025/jswd.jpg";
 
+import s4 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/2024/s.jpg";
+import as4 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/2024/as.jpg";
+import jso4 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/2024/jso.jpg";
+import jso24 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/2024/jso2.jpg";
+import jse4 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/2024/jse.jpg";
+import jsd4 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/2024/jsd.jpg";
+import jsd24 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/2024/jsd2.jpg";
+import jswd4 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/2024/jswd.jpg";
 
-import dv1 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/Development/Dv1.jpeg";
-import dv2 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/Development/Dv2.png";
-
-import ds1 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/Design/Ds1.jpeg";
-import ds2 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/Design/Ds2.jpeg";
-import ds3 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/Design/Ds3.jpeg";
-import ds4 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/Design/Ds4.jpg";
-import ds5 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/Design/Ds5.jpg";
-import ds6 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/Design/Ds6.jpg";
-
-import ed1 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/Editorial/Ed1.jpeg";
-import ed2 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/Editorial/Ed2.jpg";
-import ed3 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/Editorial/Ed3.jpeg";
-import ed4 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/Editorial/Ed4.jpg";
-import ed5 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/Editorial/Ed5.jpg";
-
-import op1 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/Operation/op1.jpg";
-import op2 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/Operation/op2.png";
-import op3 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/Operation/op3.jpg";
-import op4 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/Operation/op4.jpeg";
-import op5 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/Operation/op5.jpeg";
-import op6 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/Operation/op6.jpg";
+import jso3 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/2023/jso.jpg";
 
 import f1 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/Faculty/F1.jpg";
 import f2 from "../../../../Assets/OurTeam_imgs/OurTeam_photos/Faculty/F2.jpg";
 
-
 const OurTeamData = {
-    Secretaries : [
+    2025 : [
         {
             name: "Anushree Gupta",
             position: "Secretary",
-            img: secy,
+            img: s5,
             mail: "mailto:anushree_g@ee.iitr.ac.in",
             linkedin:"",
             insta:"",
         },
         {
             name: "Aryan Kesarwani",
-            position: "Additional Secy",
-            img: AddSecy,
+            position: "Additional Secretary",
+            img: as5,
             mail: "mailto:aryan_k@ee.iitr.ac.in",
             linkedin:"",
             insta:"",
         },
-        {
-            name: "Baldeep Chabbra",
-            position: "Alumini Outreach Secy",
-            img: AlumSecy,
-            mail: "",
-            linkedin:"",
-            insta:"",
-        },
+        
         {
             name: "Arjav Jain",
-            position: "Operations Secy",
-            img: OpSecy,
+            position: "Joint Secretary Operations",
+            img: jso5,
             mail: "",
             linkedin:"",
             insta:"",
         },
-        {
-            name: "Daksh Mehla",
-            position: "Editorial Secy",
-            img: EdSecy,
-            mail: "",
-            linkedin:"",
-            insta:"",
-        },
-        {
-            name: "Lakshya Soni",
-            position: "Design Secy",
-            img: DesignSecy,
-            mail: "",
-            linkedin:"",
-            insta:"",
-        },
-    ],
-    Development : [
         {
             name: "Ashi Gupta",
-            position: "",
-            img: dv1,
+            position: "Joint Secretary Web Development",
+            img: jswd5,
             mail: "",
             linkedin:"",
             insta:"",
         },
+       
+       
+    ],
+    2024 : [
         {
-            name: "Rahul Modi",
-            position: "",
-            img: dv2,
+            name: "Navneet Kumar",
+            position: "Secretary",
+            img: s4,
+            mail: "",
+            linkedin:"",
+            insta:"",
+        },{
+            name: "Krity Verma",
+            position: "Additional Secretary",
+            img: as4,
+            mail: "",
+            linkedin:"",
+            insta:"",
+        },{
+            name: "Paaban Panda",
+            position: "Joint Secretary Operations",
+            img: jso4,
+            mail: "",
+            linkedin:"",
+            insta:"",
+        },{
+            name: "Palak Tripathi",
+            position: "Joint Secretary Operations",
+            img: jso24,
+            mail: "",
+            linkedin:"",
+            insta:"",
+        },{
+            name: "Palak",
+            position: "Joint Secretary Editorial",
+            img: jse4,
+            mail: "",
+            linkedin:"",
+            insta:"",
+        },{
+            name: "Niharika Upveja",
+            position: "Joint Secretary Web Development",
+            img: jswd4,
+            mail: "",
+            linkedin:"",
+            insta:"",
+        },{
+            name: "Dhruv Sagar",
+            position: "Joint Secretary Design",
+            img: jsd4,
+            mail: "",
+            linkedin:"",
+            insta:"",
+        },{
+            name: "Abhay Kumar",
+            position: "Joint Secretary Design",
+            img: jsd24,
             mail: "",
             linkedin:"",
             insta:"",
         }
     ],
-    Design : [
+    2023 : [
         {
-            name: "Kavya Arora",
-            position: "",
-            img: ds1,
-            mail: "",
-            linkedin:"",
-            insta:"",
-        },
-        {
-            name: "Piyush Chittora",
-            position: "",
-            img: ds2,
-            mail: "",
-            linkedin:"",
-            insta:"",
-        },
-        {
-            name: "Ishita Garg",
-            position: "",
-            img: ds3,
-            mail: "",
-            linkedin:"",
-            insta:"",
-        },
-        {
-            name: "Shreya Attri",
-            position: "",
-            img: ds4,
-            mail: "",
-            linkedin:"",
-            insta:"",
-        },
-        {
-            name: "Saransh Gaur",
-            position: "",
-            img: ds5,
-            mail: "",
-            linkedin:"",
-            insta:"",
-        },
-        {
-            name: "Dhairya Modi",
-            position: "",
-            img: ds6,
-            mail: "",
-            linkedin:"",
-            insta:"",
-        },
-    ],
-    Editorial : [
-        {
-            name: "Abhiyansh Raj",
-            position: "",
-            img: ed1,
-            mail: "",
-            linkedin:"",
-            insta:"",
-
-        },
-        {
-            name: "Harshvardhan",
-            position: "",
-            img: ed2,
-            mail: "",
-            linkedin:"",
-            insta:"",
-        },
-        {
-            name: "Mohit Singh Guleria",
-            position: "",
-            img: ed3,
-            mail: "",
-            linkedin:"",
-            insta:"",
-        },
-        {
-            name: "Vardaan Jain",
-            position: "",
-            img: ed4,
-            mail: "",
-        },
-        {
-            name: "Nikita Raj",
-            position: "",
-            img: ed5,
-            mail: "",
-            linkedin:"",
-            insta:"",
-        }
-    ],
-    Operations : [
-        {
-            name: "Somil Dhiman",
-            position: "",
-            img: op1,
-            mail: "",
-            linkedin:"",
-            insta:"",
-        },
-        {
-            name: "Devesh Rathore",
-            position: "",
-            img: op2,
-            mail: "",
-            linkedin:"",
-            insta:"",
-        },
-        {
-            name: "Harshvarshan Raghu",
-            position: "",
-            img: op3,
-            mail: "",
-            linkedin:"",
-            insta:"",
-        },
-        {
-            name: "Ramandeep Singh",
-            position: "",
-            img: op4,
-            mail: "",
-            linkedin:"",
-            insta:"",
-        },
-        {
-            name: "Sanskriti Sharan",
-            position: "",
-            img: op5,
-            mail: "",
-            linkedin:"",
-            insta:"",
-        },
-        {
-            name: "Rishabh Dubey",
-            position: "",
-            img: op6,
+            name: "Bhargav Pratim Nath",
+            position: "Joint Secretary Operations",
+            img: jso3,
             mail: "",
             linkedin:"",
             insta:"",
--- src/components/About/OurTeam/OurTeam_data/OurTeam_data.jsx
+++ src/components/About/OurTeam/OurTeam_data/OurTeam_data.jsx
@@ -1,31 +1,73 @@
-import React from 'react'
-import './OurTeam_data.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "./OurTeam_data.css";
 import OurTeamData from "./OurTeamData.jsx";
-import Insta_Icon from '../../../../Assets/OurTeam_imgs/OurTeam_icons/Insta_icon';
-import Linkedin_Icon from '../../../../Assets/OurTeam_imgs/OurTeam_icons/Linkedin_icon';
-import Mail_Icon from '../../../../Assets/OurTeam_imgs/OurTeam_icons/Mail_icon';
+import Insta_Icon from "../../../../Assets/OurTeam_imgs/OurTeam_icons/Insta_icon";
+import Linkedin_Icon from "../../../../Assets/OurTeam_imgs/OurTeam_icons/Linkedin_icon";
+import Mail_Icon from "../../../../Assets/OurTeam_imgs/OurTeam_icons/Mail_icon";
 
 export default function OurTeam_data({ team }) {
+  // Guard against missing team or invalid data to avoid runtime errors
+  const members = Array.isArray(OurTeamData[team]) ? OurTeamData[team] : [];
+
   return (
     <div id="team" className="ourTeam__container">
       <div className="ourTeam__member">
-        {OurTeamData[team].map((obj, indx) => {
-          return (
-            <div key={indx} className="ourTeam__teamCard">
-              <div className="img_container">
-                <img src={obj.img} alt={obj.name} />
-                <div className="img_hover_content">
-                  <div className="content_mail"><a href={obj.mail} target="blank"><Mail_Icon/></a></div>
-                  <div className="content_linkedin"><a href={obj.linkedin} target="blank"><Linkedin_Icon/></a></div>
-                  <div className="content_insta"><a href={obj.insta} target="blank"><Insta_Icon/></a></div>
+        {members.length === 0 ? (
+          <div className="ourTeam__noMembers">No members to display.</div>
+        ) : (
+          members.map((obj, indx) => {
+            return (
+              <div key={indx} className="ourTeam__teamCard">
+                <div className="img_container">
+                  <img src={obj.img} alt={obj.name} />
+                  <div className="img_hover_content">
+                    {obj.mail && (
+                      <div className="content_mail">
+                        <a
+                          href={obj.mail}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <Mail_Icon />
+                        </a>
+                      </div>
+                    )}
+                    {obj.linkedin && (
+                      <div className="content_linkedin">
+                        <a
+                          href={obj.linkedin}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <Linkedin_Icon />
+                        </a>
+                      </div>
+                    )}
+                    {obj.insta && (
+                      <div className="content_insta">
+                        <a
+                          href={obj.insta}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <Insta_Icon />
+                        </a>
+                      </div>
+                    )}
+                  </div>
                 </div>
+                <div className="ourTeam__memberName">{obj.name}</div>
+                <div className="ourTeam__memberPos">{obj.position}</div>
               </div>
-              <div className="ourTeam__memberName">{obj.name}</div>
-              <div className="ourTeam__memberPos">{obj.position}</div>
-            </div>
-          );
-        })}
+            );
+          })
+        )}
       </div>
     </div>
-  )
+  );
 }
+
+OurTeam_data.propTypes = {
+  team: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
